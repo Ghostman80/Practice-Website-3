e377f8ef74518f964219801d4c128096e2a12c0e
Jared Johnson <60166120+Ghostman80@users.noreply.github.com>
2020-12-03 09:51:25 -0700

    Add files via upload
    Website information

---
+++ main.js
@@ -0,0 +1,6 @@
+const favGame = ['Megaman', 'Castlevania', 'Metroid', 'Super Mario 2', 'Contra'];
+const favCheese = ['Cheedar', 'Swiss', 'American', 'Parmesan', 'Feta'];
+
+console.log(favGame);
+console.log(favCheese);
+
